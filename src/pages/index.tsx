bd0a40600ec04571563157e842694b70294472f7
Thales Kenne <thales.kenne@altimus.com.br>
2021-04-21 16:46:49 -0300

    Formatting data

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,28 @@
-import { Header } from "../components/Header";
+import { GetStaticProps } from 'next';
+import { format, parseISO } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
+import api from '../services/api';
+import { convertDurationToTimeString } from '../utils/convertDurationToTimeString';
 
-export default function Home() {
+type Episode = {
+  id: string;
+  title: string;
+  thumbnail: string;
+  description: string;
+  members: string;
+  duration: string;
+  durationAsString: string;
+  url: string;
+  publishedAt: string;
+}
+
+type HomeProps = {
+  episodes: Episode[]
+}
+
+export default function Home(props: HomeProps) {
   return (
-    <h1>whahh</h1>
+    <p>{JSON.stringify(props.episodes[0])}</p>
   )
 }
 
@@ -17,13 +37,32 @@ create a static html and every single access within a set span of time
  will be served this static page.
 */
 
-export async function getStaticProps() {
-  const response = await fetch('http://localhost:3333/episodes')
-  const data = await response.json();
+export const getStaticProps: GetStaticProps = async () => {
+  const { data } = await api.get('episodes', {
+    params: {
+      _limit: 12,
+      _sort: 'published_at',
+      _order: 'desc'
+    }
+  })
+
+  const episodes = data.map((episode) => {
+    return {
+      id: episode.id,
+      title: episode.title,
+      thumbnail: episode.thumbnail,
+      members: episode.members,
+      publishedAt: format(parseISO(episode.published_at), 'd MMM yy', { locale: ptBR }),
+      duration: Number(episode.file.duration),
+      description: episode.description,
+      url: episode.file.url,
+      durationAsString: convertDurationToTimeString(Number(episode.file.duration))
+    }
+  })
 
   return {
     props: {
-      episodes: data
+      episodes
     },
     revalidate: 60 * 60 * 8,
   }
