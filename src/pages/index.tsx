c60af3b38884c9db20623ca7b8ca6e0c20bcd77d
Thales Kenne <thales.kenne@altimus.com.br>
2021-04-21 12:18:59 -0300

    Adding SSG

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,3 +5,48 @@ export default function Home() {
     <h1>whahh</h1>
   )
 }
+
+/*
+By declaring "getStaticProps", Nextjs will understand 
+that it needs to fetch that information before the app even renders
+so that crawlers will be able to see that info on first render
+and index your page.
+
+This is called Static Site Generation. The first access to the page will
+create a static html and every single access within a set span of time
+ will be served this static page.
+*/
+
+export async function getStaticProps() {
+  const response = await fetch('http://localhost:3333/episodes')
+  const data = await response.json();
+
+  return {
+    props: {
+      episodes: data
+    },
+    revalidate: 60 * 60 * 8,
+  }
+}
+
+
+/*
+By declaring "getServerSideProps", Nextjs will understand
+that it needs to fetch that information before the app even renders
+so that crawlers will be able to see that info on first render
+and index your page.
+
+This is called Server Side Rendering. Every single access to the page
+will run this function.
+*/
+
+// export async function getServerSideProps() {
+//   const response = await fetch('http://localhost:3333/episodes')
+//   const data = await response.json();
+
+//   return {
+//     props: {
+//       episodes: data
+//     }
+//   }
+// }
